f43360f59ffc06b4a277d36a9ef70d8894a5726a
RitikaSingh25 <49776837+ritikasingh25@users.noreply.github.com>
2021-05-14 20:25:17 +0530

    Add files via upload

---
+++ products.php
@@ -0,0 +1,301 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width", initial-scale=10.>
+	<title>All  Products - OnlineSliverCreek </title>
+	<link rel="stylesheet" href="style1.css">
+	<link
+		href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,200;0,300;0,500;1,600;1,700;1,800&display=swap"
+		rel="stylesheet">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+
+<body>
+	<div class="header">
+
+		<div class="container">
+			<div class="navbar">
+				<div class="logo">
+	
+					<img src="images/logo.jpeg" width="145px">
+				</div>
+				<nav>
+					<ul id="MenuItems">
+						<li><a href="index.php">Home</a></li>
+						<li><a href="registration.php">Register</a></li>
+						<li><a href="about.php">About</a></li>
+						<li><a href="products.php">Products</a></li>
+						<li><a href="login.php">Login</a></li>
+					</ul>
+				</nav>
+
+				<a class="nav-link" href="cart.php"><img src="https://img.icons8.com/color/30/ffffff/shopping-cart--v1.png"/></a>
+				<img src="img/menu.png" class="menu-icon" onclick="menutoggle()">
+			</div>
+			
+		</div>
+	</div>
+	
+	  
+	
+
+
+	<div class="small-container">
+		<div class="row row-2">
+			<h2>All  Products</h2>
+			<select>
+				<option>Default Shorting</option>
+				<option>Short by price</option>
+				<option>Short by popularity</option>
+				<option>Short by rating</option>
+				<option>Short by sale</option>
+			</select>
+		</div>
+		<div class="row">
+			<div class="colu-4">
+				<a href="productsdetails.php"><img src="images/pro1 (1).jpg"></a>
+				<a href="productsdetails.php"><h4>Green Printed T-shirt</h4></a>
+				<div class="rating">
+					<i class="fa fa-star"></i>
+					<i class="fa fa-star"></i>
+					<i class="fa fa-star"></i>
+					<i class="fa fa-star"></i>
+					<i class="fa fa-star-o"></i>
+
+
+
+				</div>
+				<p>$200.00</p>
+			</div>
+			<div class="colu-4">
+				<img src="images/gtra.jpeg">
+				<h4>Women's Tracksuit</h4>
+				<div class="rating">
+					<i class="fa fa-star"></i>
+					<i class="fa fa-star"></i>
+					<i class="fa fa-star"></i>
+					<i class="fa fa-star"></i>
+					<i class="fa fa-star-o"></i>
+
+
+
+				</div>
+				<p>$900.00</p>
+			</div>
+			<div class="colu-4">
+				<a href="productsdetails2.php"><img src="images/trad.jpeg"></a>
+				<a href="productsdetails2.php"><h4>Treadmil Machine</h4></a>
+				<div class="rating">
+					<i class="fa fa-star"></i>
+					<i class="fa fa-star"></i>
+					<i class="fa fa-star"></i>
+					<i class="fa fa-star"></i>
+					<i class="fa fa-star-o"></i>
+
+
+
+				</div>
+				<p>$25000.00</p>
+			</div>
+	
+			<div class="row">
+				<div class="colu-4">
+					<img src="images/lif.jpeg">
+					<h4>Weight Lifting</h4>
+					<div class="rating">
+						<i class="fa fa-star"></i>
+						<i class="fa fa-star"></i>
+						<i class="fa fa-star"></i>
+						<i class="fa fa-star"></i>
+						<i class="fa fa-star-o"></i>
+	
+	
+	
+					</div>
+					<p>$1200.00</p>
+				</div>
+				<div class="colu-4">
+					<img src="images/dry.jpeg">
+					<h4>Dry Fruit</h4>
+					<div class="rating">
+						<i class="fa fa-star"></i>
+						<i class="fa fa-star"></i>
+						<i class="fa fa-star"></i>
+						<i class="fa fa-star"></i>
+						<i class="fa fa-star-o"></i>
+	
+	
+	
+					</div>
+					<p>$900.00</p>
+				</div>
+				<div class="colu-4">
+					<img src="images/pro.jpeg">
+					<h4> Protein Shake</h4>
+					<div class="rating">
+						<i class="fa fa-star"></i>
+						<i class="fa fa-star"></i>
+						<i class="fa fa-star"></i>
+						<i class="fa fa-star"></i>
+						<i class="fa fa-star-o"></i>
+	
+	
+	
+					</div>
+					<p>$100.00</p>
+				</div>
+				<div class="colu-4">
+					<img src="images/prot.jpeg">
+					<h4>Whey Protein Shake</h4>
+					<div class="rating">
+						<i class="fa fa-star"></i>
+						<i class="fa fa-star"></i>
+						<i class="fa fa-star"></i>
+						<i class="fa fa-star"></i>
+						<i class="fa fa-star"></i>
+	
+	
+	
+					</div>
+					<p>$250.00</p>
+				</div>
+				<div class="colu-4">
+					<img src="images/track.jpeg">
+					<h4>Men's Tracksuit</h4>
+					<div class="rating">
+						<i class="fa fa-star"></i>
+						<i class="fa fa-star"></i>
+						<i class="fa fa-star"></i>
+						<i class="fa fa-star"></i>
+						<i class="fa fa-star-o"></i>
+	
+	
+	
+					</div>
+					<p>$6000.00</p>
+				</div>
+				<div class="colu-4">
+					<img src="images/trac.jpeg">
+					<h4>Grey Tracksuit </h4>
+					<div class="rating">
+						<i class="fa fa-star"></i>
+						<i class="fa fa-star"></i>
+						<i class="fa fa-star"></i>
+						<i class="fa fa-star-o"></i>
+						<i class="fa fa-star-o"></i>
+	
+	
+	
+					</div>
+					<p>$1200.00</p>
+				</div>
+				<div class="colu-4">
+					<img src="images/rosta.jpeg">
+					<h4>Pistachios</h4>
+					<div class="rating">
+						<i class="fa fa-star"></i>
+						<i class="fa fa-star"></i>
+						<i class="fa fa-star"></i>
+						<i class="fa fa-star"></i>
+						<i class="fa fa-star-o"></i>
+	
+	
+	
+					</div>
+					<p>$800.00</p>
+				</div>
+				<div class="colu-4">
+					<img src="images/fru.jpeg">
+					<h4>Apricots</h4>
+					<div class="rating">
+						<i class="fa fa-star"></i>
+						<i class="fa fa-star"></i>
+						<i class="fa fa-star"></i>
+						<i class="fa fa-star-o"></i>
+						<i class="fa fa-star-o"></i>
+	
+	
+	
+					</div>
+					<p>$600.00</p>
+				</div>
+			</div>
+		</div>
+		<div class="page-btn">
+		    <span>1</span>
+			<a href="moreproducts.php"><span>2</span>
+			<span>3</span>
+			<span>4</span>
+			<span>&#8594;</span>
+
+		</div>
+		
+    </div>
+    <!----------------footer---------->
+			<div class="footer">
+				<div class="container">
+					<div class="row">
+						<div class="footer-colu-1">
+							<h3>Download Our App</h3>
+							<p>Download App for Android and ios mobile phone.</p>
+							<div class="app-logo">
+								<img src="images/app-logo.png">
+								<img src="images/app.png">
+							</div>
+						</div>
+						<div class="footer-colu-2">
+							<img src="images/log.png">
+							<p>Our Purpose Is To Sustainably Make the Pleasure and
+								Benefits of Sports Accessible to the Many.
+							</p>
+						</div>
+						<div class="footer-colu-3">
+							<h3>Useful Links</h3>
+							<ul>
+								<li>Coupons</li>
+								<li>Blog Post</li>
+								<li>Return Policy</li>
+								<li>Join Affiliate</li>
+
+							</ul>
+						</div>
+						<div class="footer-colu-4">
+							<h3>Follow us</h3>
+							<ul>
+								<li>Facebook</li>
+								<li>Twitter</li>
+								<li>Instagram</li>
+								<li>YouTube</li>
+
+							</ul>
+						</div>
+					</div>
+					<hr>
+					<p class="Copyright">Conditions of use Privacy Notice Intrested-Based Ads &#169 2021,
+						OnlineSilverCreek.com </p>
+				</div>
+			</div>
+<!----------------js for toggle menu-------------->
+
+<script>
+	var MenuItems = document.getElementById("MenuItems");
+	MenuItems.style.maxHeight = "0px";
+	function menutoggle(){
+		if(MenuItems.style.maxHeight == "0px"){
+			MenuItems.style.maxHeight = "200px";
+		}
+		else{
+			MenuItems.style.maxHeight = "0px";
+		}
+	}
+</script>
+
+
+
+
+
+</body>
+
+</html>
