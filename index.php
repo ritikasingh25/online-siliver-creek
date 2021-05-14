3f720040c1b4aafcac7775fba75132e23b3a1065
RitikaSingh25 <49776837+ritikasingh25@users.noreply.github.com>
2021-05-14 20:24:53 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,439 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+
+
+    <title>Online Silver Creek</title>
+	<link rel="stylesheet" href="style1.css">
+	<link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,200;0,300;0,500;1,600;1,700;1,800&display=swap"
+		rel="stylesheet">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+  </head>
+     
+<body>
+<div class="header">
+
+<div class="container">
+	<div class="navbar">
+		<div class="logo">
+
+			<img src="images/logo.jpeg" width="145px">
+		</div>
+		<nav>
+			<ul id="MenuItems">
+				<li><a href="index.php">Home</a></li>
+				<li><a href="registration.php">Register</a></li>
+				<li><a href="about.php">About</a></li>
+				<li><a href="products.php">Products</a></li>
+				<li><a href="login.php">Login</a></li>
+			</ul>
+		</nav>
+		<a class="nav-link" href="cart.php"><img src="https://img.icons8.com/color/30/ffffff/shopping-cart--v1.png"/></a>
+		<img src="img/menu.png" class="menu-icon" onclick="menutoggle()">
+	</div>
+</div>
+<div class="row">
+	<div class="colu-2">
+		<h1>OUTLINE OF SPORTS AND FITNESS!</h1>
+		<p>"Trending Sportswear",
+			Gym Equipments For Iron Benders,
+			Notable high-protein diets, and
+			Effective Products Against COVID-19.
+			"There are no secrets to success. It is the result of preparation,<br>hard work, and learning
+			from failure."<p>
+		<a href="" class="btn">Explore Now &#8594;</a>
+
+	</div>
+		<div class="colu-2">
+			<img src="images/backimg.png">
+				
+			</div>
+		</div>
+		</div>
+	</div>
+
+
+	
+	<!----------featured categories-------------->
+	<div class="categories">
+		<div class="small-container">
+			<div class="row">
+				<div class="colu-3">
+					<img src="images/cat1.jpg">
+
+				</div>
+				<div class="colu-3">
+					<img src="images/diet.jpg">
+
+				</div>
+				<div class="colu-3">
+					<img src="images/cat3.jpg">
+
+				</div>
+
+			</div>
+		</div>
+
+	</div>
+	<!----------featured products-------------->
+	<div class="small-container">
+		<h2 class="title">Featured Products</h2>
+		<div class="row">
+			<div class="colu-4">
+				<a href="productsdetails.php"><img src="images/pro1 (1).jpg"></a>
+				<a href="productsdetails.php"><h4>Green Printed T-shirt</h4></a>
+				<div class="rating">
+					<i class="fa fa-star"></i>
+					<i class="fa fa-star"></i>
+					<i class="fa fa-star"></i>
+					<i class="fa fa-star"></i>
+					<i class="fa fa-star-o"></i>
+
+
+
+				</div>
+				<p>$500.00</p>
+			</div>
+			<div class="colu-4">
+				<img src="images/prot.png">
+				<h4> Protein Shake</h4>
+				<div class="rating">
+					<i class="fa fa-star"></i>
+					<i class="fa fa-star"></i>
+					<i class="fa fa-star"></i>
+					<i class="fa fa-star"></i>
+					<i class="fa fa-star-o"></i>
+
+
+
+				</div>
+				<p>$100.00</p>
+			</div>
+			<div class="colu-4">
+				<img src="images/trad.jpg">
+				<h4>Treadmill</h4>
+				<div class="rating">
+					<i class="fa fa-star"></i>
+					<i class="fa fa-star"></i>
+					<i class="fa fa-star"></i>
+					<i class="fa fa-star"></i>
+					<i class="fa fa-star-o"></i>
+
+
+
+				</div>
+				<p>$40,500.00</p>
+			</div>
+			<div class="colu-4">
+				<img src="images/pro.jpg">
+				<h4>Gold Standard 100 Whey Protein Shake</h4>
+				<div class="rating">
+					<i class="fa fa-star"></i>
+					<i class="fa fa-star"></i>
+					<i class="fa fa-star"></i>
+					<i class="fa fa-star"></i>
+					<i class="fa fa-star-o"></i>
+
+
+
+				</div>
+				<p>$1500.00</p>
+			</div>
+		</div>
+		<h2 class="title">Latest Products</h2>
+		<div class="row">
+			<div class="colu-4">
+				<img src="images/pro5.jpg">
+				<h4>Women Grey & Black Tracksuit</h4>
+				<div class="rating">
+					<i class="fa fa-star"></i>
+					<i class="fa fa-star"></i>
+					<i class="fa fa-star"></i>
+					<i class="fa fa-star"></i>
+					<i class="fa fa-star-o"></i>
+
+
+
+				</div>
+				<p>$1200.00</p>
+			</div>
+			<div class="colu-4">
+				<img src="images/pro6.jpg">
+				<h4>Women Red & White Sports Shoes</h4>
+				<div class="rating">
+					<i class="fa fa-star"></i>
+					<i class="fa fa-star"></i>
+					<i class="fa fa-star"></i>
+					<i class="fa fa-star"></i>
+					<i class="fa fa-star-o"></i>
+
+
+
+				</div>
+				<p>$900.00</p>
+			</div>
+			<div class="colu-4">
+				<img src="images/pro13.png">
+				<h4>Men's Cotton Cloth Face Mask</h4>
+				<div class="rating">
+					<i class="fa fa-star"></i>
+					<i class="fa fa-star"></i>
+					<i class="fa fa-star"></i>
+					<i class="fa fa-star"></i>
+					<i class="fa fa-star-o"></i>
+
+
+
+				</div>
+				<p>$100.00</p>
+			</div>
+			<div class="colu-4">
+				<img src="images/pro11.jpg">
+				<h4>Purell Advanced Hand Sanitizer</h4>
+				<div class="rating">
+					<i class="fa fa-star"></i>
+					<i class="fa fa-star"></i>
+					<i class="fa fa-star"></i>
+					<i class="fa fa-star"></i>
+					<i class="fa fa-star"></i>
+
+
+
+				</div>
+				<p>$150.00</p>
+			</div>
+			<div class="colu-4">
+				<img src="images/watch.jpg">
+				<h4>Edifice Analogue Men's Watch</h4>
+				<div class="rating">
+					<i class="fa fa-star"></i>
+					<i class="fa fa-star"></i>
+					<i class="fa fa-star"></i>
+					<i class="fa fa-star"></i>
+					<i class="fa fa-star-o"></i>
+
+
+
+				</div>
+				<p>$8000.00</p>
+			</div>
+			<div class="colu-4">
+				<img src="images/gym.jpg">
+				<h4>WELCARE Cardio Equipment</h4>
+				<div class="rating">
+					<i class="fa fa-star"></i>
+					<i class="fa fa-star"></i>
+					<i class="fa fa-star"></i>
+					<i class="fa fa-star-o"></i>
+					<i class="fa fa-star-o"></i>
+
+
+
+				</div>
+				<p>$15,000</p>
+			</div>
+			<div class="colu-4">
+				<img src="images/dry.jpg">
+				<h4>Dry Fruits</h4>
+				<div class="rating">
+					<i class="fa fa-star"></i>
+					<i class="fa fa-star"></i>
+					<i class="fa fa-star"></i>
+					<i class="fa fa-star"></i>
+					<i class="fa fa-star-o"></i>
+
+
+
+				</div>
+				<p>$800.00</p>
+			</div>
+			<div class="colu-4">
+				<img src="images/pro7.jpg">
+				<h4>Black Trousers</h4>
+				<div class="rating">
+					<i class="fa fa-star"></i>
+					<i class="fa fa-star"></i>
+					<i class="fa fa-star"></i>
+					<i class="fa fa-star-o"></i>
+					<i class="fa fa-star-o"></i>
+
+
+
+				</div>
+				<p>$600.00</p>
+			</div>
+		</div>
+	</div>
+	<!--------offer------------->
+	<div class="offer">
+		<div class="small-container">
+			<div class="row">
+				<div class="colu-2">
+					<img src="images/smart.jpg" class="offer-img">
+				</div>
+				<div class="colu-2">
+					<p>"Exclusively Available on OnlineStore"</p>
+					<h1>Smart brand 4</h1>
+					<small>The Mi Smart Band 4 features a 39.9% larger (than Mi Band 3)
+						AMOLED color full-touch display with adjustable brightness,
+						so everything is clear as can be.
+					</small>
+					<a href="" class="btn">Buy Now &#8594;</a>
+
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<!--------------------testimonial-------------->
+	<div class="testimonial">
+		<div class="small-container">
+			<div class="row">
+				<div class="colu-3">
+					<i class="fa fa-quote-left"></i>
+					<p>Lorem Ipsum is simply dummy text of the printing
+						and typesetting industry. Lorem Ipsum has been the
+						industry's standard dummy text ever
+					</p>
+					<div class="rating">
+						<i class="fa fa-star"></i>
+						<i class="fa fa-star"></i>
+						<i class="fa fa-star"></i>
+						<i class="fa fa-star"></i>
+						<i class="fa fa-star-o"></i>
+					</div>
+					<img src="images/user.jpg">
+					<h3>Sean Parker</h3>
+
+				</div>
+				<div class="colu-3">
+					<i class="fa fa-quote-left"></i>
+					<p>Lorem Ipsum is simply dummy text of the printing
+						and typesetting industry. Lorem Ipsum has been the
+						industry's standard dummy text ever
+					</p>
+					<div class="rating">
+						<i class="fa fa-star"></i>
+						<i class="fa fa-star"></i>
+						<i class="fa fa-star"></i>
+						<i class="fa fa-star"></i>
+						<i class="fa fa-star-o"></i>
+					</div>
+					<img src="images/user2.jpg">
+					<h3>Mike Smith</h3>
+
+				</div>
+				<div class="colu-3">
+					<i class="fa fa-quote-left"></i>
+					<p>Lorem Ipsum is simply dummy text of the printing
+						and typesetting industry. Lorem Ipsum has been the
+						industry's standard dummy text ever
+					</p>
+					<div class="rating">
+						<i class="fa fa-star"></i>
+						<i class="fa fa-star"></i>
+						<i class="fa fa-star"></i>
+						<i class="fa fa-star"></i>
+						<i class="fa fa-star-o"></i>
+					</div>
+					<img src="images/user3.jpg">
+					<h3>Mabel Joe</h3>
+
+				</div>
+			</div>
+		</div>
+
+		<!------------brands--------------->
+		<div class="brands">
+			<div class="small-container">
+				<div class="row">
+					<div class="colu-5">
+						<img src="images/brand.jpg">
+					</div>
+					<div class="colu-5">
+						<img src="images/brand1.jpg">
+					</div>
+					<div class="colu-5">
+						<img src="images/brand2.jpg">
+					</div>
+					<div class="colu-5">
+						<img src="images/brand3.jpg">
+					</div>
+					<div class="colu-5">
+						<img src="images/paytm.jpg">
+					</div>
+				</div>
+			</div>
+			<!----------------footer---------->
+			
+			<div class="footer">
+				<div class="container">
+					<div class="row">
+						<div class="footer-colu-1">
+							<h3>Download Our App</h3>
+							<p>Download App for Android and ios mobile phone.</p>
+							<div class="app-logo">
+								<img src="images/app-logo.png">
+								<img src="images/app.png">
+							</div>
+						</div>
+						<div class="footer-colu-2">
+							<img src="images/log.png">
+							<p>Our Purpose Is To Sustainably Make the Pleasure and
+								Benefits of Sports Accessible to the Many.
+							</p>
+						</div>
+						<div class="footer-colu-3">
+							<h3>Useful Links</h3>
+							<ul>
+								<li>Coupons</li>
+								<li>Blog Post</li>
+								<li>Return Policy</li>
+								<li>Join Affiliate</li>
+
+							</ul>
+						</div>
+						<div class="footer-colu-4">
+							<h3>Follow us</h3>
+							<ul>
+								<li>Facebook</li>
+								<li>Twitter</li>
+								<li>Instagram</li>
+								<li>YouTube</li>
+
+							</ul>
+						</div>
+					</div>
+					<hr>
+					<p class="Copyright">Conditions of use Privacy Notice Intrested-Based Ads &#169 2021,
+						OnlineSilverCreek.com </p>
+				</div>
+			</div>
+
+<!----------------js for toggle menu-------------->
+
+<script>
+	var MenuItems = document.getElementById("MenuItems");
+	MenuItems.style.maxHeight = "0px";
+	function menutoggle(){
+		if(MenuItems.style.maxHeight == "0px"){
+			MenuItems.style.maxHeight = "200px";
+		}
+		else{
+			MenuItems.style.maxHeight = "0px";
+		}
+	}
+</script>
+
+
+
+
+
+</body>
+
+</html>
